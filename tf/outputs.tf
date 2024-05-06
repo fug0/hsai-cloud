6612a8c22333b0a7fdabdf1cae2de22a73c59c3e
Egor Belyaev <egor.belyaev@nnz.ru>
2024-05-06 22:40:57 +0300

    revert commit

--- tf/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "servers" {
-  value = {
-    event_reminder_server = openstack_compute_instance_v2.event_reminder_server.access_ip_v4
-    event_reminder_db_server = openstack_compute_instance_v2.event_reminder_db_server.access_ip_v4
-  }
-}
