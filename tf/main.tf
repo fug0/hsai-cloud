21066614640471ddc2504f221961f4c5345bb5aa
fug0 <eleectricgore@gmail.com>
2024-05-08 01:11:21 +0300

    added docker deployment files

--- tf/main.tf
+++ tf/main.tf
@@ -19,43 +19,21 @@ provider "openstack" {
 }
 
 resource "openstack_networking_secgroup_v2" "sg" {
-  name        = "event_reminder_sg"
+  name        = "event_reminder_docker_sg"
 }
 
 resource "openstack_networking_secgroup_rule_v2" "sg_rule_ssh" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
+  port_range_min    = 1
+  port_range_max    = 20000
   remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = openstack_networking_secgroup_v2.sg.id
 }
 
-resource "openstack_networking_secgroup_rule_v2" "sg_rule_sql" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 3306
-  port_range_max    = 3306
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.sg.id
-}
-
-resource "openstack_compute_instance_v2" "event_reminder_server" {
+resource "openstack_compute_instance_v2" "event_reminder_docker_server" {
   name        = "event_reminder_server"
-  image_name  = var.image_name
-  flavor_name = var.flavor_name
-  key_pair = var.key_pair
-  security_groups = [openstack_networking_secgroup_v2.sg.name]
-
-  network {
-    name = var.network_name
-  }
-}
-
-resource "openstack_compute_instance_v2" "event_reminder_db_server" {
-  name        = "event_reminder_db_server"
   image_name  = var.image_name
   flavor_name = var.flavor_name
   key_pair = var.key_pair
